ca606849ae6457e28eda57dcdd48aa10e01ef7f6
Tom Jones <thjones76@outlook.com>
2024-09-05 13:48:24 +0100

    London / New York time lib additions

--- Utils/Time/Time.cs
+++ Utils/Time/Time.cs
@@ -2,10 +2,41 @@
 {
     public class Time
     {
-        // Example method
-        public static string GetCurrentTime()
+        private TimeZoneInfo londonTimeZone;
+        private TimeZoneInfo newYorkTimeZone;
+
+        public Time()
         {
-            return DateTime.Now.ToString("T");
+            londonTimeZone = TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time");
+            newYorkTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+        }
+
+        public TimeSpan GetLondonTime(DateTime serverTime)
+        {
+            DateTime londonTime = TimeZoneInfo.ConvertTime(serverTime, londonTimeZone);
+            return londonTime.TimeOfDay;
+        }
+
+        public TimeSpan GetNewYorkTime(DateTime serverTime)
+        {
+            DateTime newYorkTime = TimeZoneInfo.ConvertTime(serverTime, newYorkTimeZone);
+            return newYorkTime.TimeOfDay;
+        }
+
+        public bool IsLondonTradingSession(DateTime serverTime)
+        {
+            TimeSpan currentTimeInLondon = GetLondonTime(serverTime);
+            TimeSpan londonStart = new TimeSpan(8, 0, 0);
+            TimeSpan londonEnd = new TimeSpan(17, 0, 0);
+            return currentTimeInLondon >= londonStart && currentTimeInLondon <= londonEnd;
+        }
+
+        public bool IsNewYorkTradingSession(DateTime serverTime)
+        {
+            TimeSpan currentTimeInNewYork = GetNewYorkTime(serverTime);
+            TimeSpan newYorkStart = new TimeSpan(8, 0, 0);
+            TimeSpan newYorkEnd = new TimeSpan(17, 0, 0);
+            return currentTimeInNewYork >= newYorkStart && currentTimeInNewYork <= newYorkEnd;
         }
     }
 }
